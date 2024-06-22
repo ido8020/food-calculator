cefad7b7139a9914108b97d4605acc48351badd2
ido <rhrenrk@gmail.com>
2024-06-22 15:01:05 +0900

    3

---
+++ Health/src/main/java/com/health/member/MemberVO.java
@@ -0,0 +1,13 @@
+package com.health.member;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private int memberidx;
+	private String id;
+	private String password;
+	private String role;
+	private String name;
+	
+}
